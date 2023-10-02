68e0cd463f25d137be0e964032790629722fafed
Abishekwolvie <abishekmessi123@gmail.com>
2023-10-02 20:13:41 +0530

    added home view insert update and delete components and added the router link in appcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InsertComponent } from './insert/insert.component';
+import { ViewComponent } from './view/view.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InsertComponent,
+    ViewComponent,
+    UpdateComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
